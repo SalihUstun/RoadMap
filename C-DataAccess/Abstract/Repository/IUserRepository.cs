8a4e1fb99c36019897d7d537864fd6af1687b261
Emre <122934395+EmreGulek@users.noreply.github.com>
2025-05-23 16:17:53 +0300

    Delete C-DataAccess directory

--- C-DataAccess/Abstract/Repository/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-using Entities;
-
-namespace DataAccess.Abstract.Repository;
-
-public interface IUserRepository : IBaseRepository<User>
-{
-    Task<User?> GetByEmailWithRolesAsync(string email);
-}
--- C-DataAccess/Entities/Role.cs
+++
@@ -1,9 +0,0 @@
-namespace Entities;
-
-public class Role
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-
-    public ICollection<UserRole> UserRoles { get; set; }
-}
