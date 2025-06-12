fc9d56e02d6b97ed9829f496a1c6dc6421c4c58e
SalihUstun <m.salih5765@gmail.com>
2025-06-12 14:27:38 +0300

    -

--- roadmap/src/main.jsx
+++ roadmap/src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Router'ı ekledik
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import 'leaflet/dist/leaflet.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
